814886c81b09e975b3163dad38a7b63454e515d1
whiskermrr <witek119@gmail.com>
2018-12-14 12:08:46 +0100

    added median filter

--- app/src/main/java/com/whisker/mrr/filter_effects/BitmapFilters.kt
+++ app/src/main/java/com/whisker/mrr/filter_effects/BitmapFilters.kt
@@ -2,27 +2,26 @@ package com.whisker.mrr.filter_effects
 
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.Matrix
+import android.util.Log
 import io.reactivex.Single
 
 class BitmapFilters {
 
     companion object {
 
-        fun smoothingFilter(bitmap: Bitmap) : Single<Bitmap> {
-            val scaledBitmap = scaleBitmap(bitmap)
+        fun boxBlur(bitmap: Bitmap) : Single<Bitmap> {
+            val scaledBitmap = getResizedBitmap(bitmap)
             val imageBitmap = scaledBitmap.copy(Bitmap.Config.ARGB_8888, true)
+            val pixels = IntArray(imageBitmap.width * imageBitmap.height)
+            val newPixels = IntArray(imageBitmap.width * imageBitmap.height)
+            scaledBitmap.getPixels(pixels, 0, imageBitmap.width, 0, 0, imageBitmap.width, imageBitmap.height)
             scaledBitmap.recycle()
-            val imageWidth = imageBitmap.width
-            val imageHeight = imageBitmap.height
-            val pixels = IntArray(imageWidth * imageHeight)
-            val newPixels = IntArray(imageWidth * imageHeight)
-            bitmap.getPixels(pixels, 0, imageWidth, 0, 0, imageWidth, imageHeight)
-            bitmap.recycle()
-            val radius = Math.min(imageWidth, imageHeight) / 150
+            val radius = Math.min(imageBitmap.width, imageBitmap.height) / 150
             val divider = (2 * radius + 1) * (2 * radius + 1)
 
-            for(i in 0 until imageHeight) {
-                for(j in 0 until imageWidth) {
+            for(i in 0 until imageBitmap.height) {
+                for(j in 0 until imageBitmap.width) {
                     var sumAlpha = 0
                     var sumRed = 0
                     var sumGreen = 0
@@ -31,8 +30,8 @@ class BitmapFilters {
                         for(n in -radius..radius) {
                             val pixelX = j + m
                             val pixelY = i + n
-                            if(pixelX in 0 until (imageWidth - 1) && pixelY in 0 until (imageHeight - 1)) {
-                                val pixel = pixels[imageWidth * pixelY + pixelX]
+                            if(pixelX in 0 until (imageBitmap.width - 1) && pixelY in 0 until (imageBitmap.height - 1)) {
+                                val pixel = pixels[imageBitmap.width * pixelY + pixelX]
                                 sumAlpha += Color.alpha(pixel)
                                 sumRed += Color.red(pixel)
                                 sumGreen += Color.green(pixel)
@@ -40,19 +39,77 @@ class BitmapFilters {
                             }
                         }
                     }
-                    val newPixel = Color.argb((sumAlpha / divider), (sumRed / divider), (sumGreen / divider), (sumBlue / divider))
-                    newPixels[imageWidth * i + j] = newPixel
+                    newPixels[imageBitmap.width * i + j] = Color.argb((sumAlpha / divider), (sumRed / divider), (sumGreen / divider), (sumBlue / divider))
                 }
             }
-            imageBitmap.setPixels(newPixels, 0, imageWidth, 0, 0, imageWidth, imageHeight)
+            imageBitmap.setPixels(newPixels, 0, imageBitmap.width, 0, 0, imageBitmap.width, imageBitmap.height)
             return Single.just(imageBitmap)
         }
 
-        private fun scaleBitmap(bitmap: Bitmap) : Bitmap {
-            val ratio = Math.min(bitmap.width, bitmap.height)
-            val newWidth = bitmap.width / ratio * 1000
-            val newHeight = bitmap.height / ratio * 1000
-            return Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, false)
+        fun medianFilter(bitmap: Bitmap) : Single<Bitmap> {
+            val scaledBitmap = getResizedBitmap(bitmap)
+            val imageBitmap = scaledBitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+            val pixels = IntArray(imageBitmap.width * imageBitmap.height)
+            val newPixels = IntArray(imageBitmap.width * imageBitmap.height)
+            scaledBitmap.getPixels(pixels, 0, imageBitmap.width, 0, 0, imageBitmap.width, imageBitmap.height)
+            scaledBitmap.recycle()
+
+            val radius = Math.min(imageBitmap.width, imageBitmap.height) / 150
+            val maskSize = 2 * radius + 1
+            val medianIndex = maskSize / 2
+
+            val medianAlphaArray = IntArray(maskSize * maskSize)
+            val medianRedArray = IntArray(maskSize * maskSize)
+            val medianGreenArray = IntArray(maskSize * maskSize)
+            val medianBlueArray = IntArray(maskSize * maskSize)
+
+            for(i in 0 until imageBitmap.height) {
+                for(j in 0 until imageBitmap.width) {
+
+                    for(m in -radius..radius) {
+                        for(n in -radius..radius) {
+                            val pixelX = j + m
+                            val pixelY = i + n
+                            val currentIndex = (radius + m) * maskSize + (radius + n)
+                            val pixel = if(pixelX in 0 until (imageBitmap.width) && pixelY in 0 until (imageBitmap.height)) {
+                                pixels[imageBitmap.width * pixelY + pixelX]
+                            } else {
+                                pixels[imageBitmap.width * i + j]
+                            }
+
+                            medianAlphaArray[currentIndex] = Color.alpha(pixel)
+                            medianRedArray[currentIndex] = Color.red(pixel)
+                            medianGreenArray[currentIndex] = Color.green(pixel)
+                            medianBlueArray[currentIndex] = Color.blue(pixel)
+                        }
+                    }
+                    medianAlphaArray.sort()
+                    medianRedArray.sort()
+                    medianGreenArray.sort()
+                    medianBlueArray.sort()
+
+                    val alpha = medianAlphaArray[medianIndex]
+                    val red = medianRedArray[medianIndex]
+                    val green = medianGreenArray[medianIndex]
+                    val blue = medianGreenArray[medianIndex]
+
+                    newPixels[imageBitmap.width * i + j] = Color.argb(alpha, red, green, blue)
+                }
+            }
+            imageBitmap.setPixels(newPixels, 0, imageBitmap.width, 0, 0, imageBitmap.width, imageBitmap.height)
+            return Single.just(imageBitmap)
+        }
+
+        private fun getResizedBitmap(bitmap: Bitmap) : Bitmap {
+            val scaleSize = if(bitmap.width > bitmap.height) bitmap.width.toFloat() / 1000 else bitmap.height.toFloat() / 1000
+
+            val scaleWidth = 1f / scaleSize
+            val scaleHeight = 1f / scaleSize
+            val matrix = Matrix()
+            matrix.postScale(scaleWidth, scaleHeight)
+
+            return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, false)
         }
     }
 }
--- app/src/main/java/com/whisker/mrr/filter_effects/MainActivity.kt
+++ app/src/main/java/com/whisker/mrr/filter_effects/MainActivity.kt
@@ -4,9 +4,11 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val PICK_IMAGE_CODE = 1001
     }
+
+    private val disposables: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +29,9 @@ class MainActivity : AppCompatActivity() {
             photoPickerIntent.type = "image/*"
             startActivityForResult(photoPickerIntent, PICK_IMAGE_CODE)
         }
+
+        ibBlur.setOnClickListener { boxBlur() }
+        ibMedian.setOnClickListener { medianFilter() }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -35,15 +42,38 @@ class MainActivity : AppCompatActivity() {
                     val imageUri = data.data!!
                     val imageStream = contentResolver.openInputStream(imageUri)
                     val imageBitmap = BitmapFactory.decodeStream(imageStream)
-
-                    BitmapFilters.smoothingFilter(imageBitmap)
-                    .subscribeOn(Schedulers.computation())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe { it ->
-                        ivSelectedImage.setImageBitmap(it)
-                    }
+                    ivSelectedImage.setImageBitmap(imageBitmap)
                 }
             }
         }
+    }
+
+    private fun boxBlur() {
+        val imageBitmap = (ivSelectedImage.drawable as BitmapDrawable).bitmap
+        disposables.add(
+                BitmapFilters.boxBlur(imageBitmap)
+                        .subscribeOn(Schedulers.computation())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe { it ->
+                            ivSelectedImage.setImageBitmap(it)
+                        }
+        )
+    }
+
+    private fun medianFilter() {
+        val imageBitmap = (ivSelectedImage.drawable as BitmapDrawable).bitmap
+        disposables.add(
+                BitmapFilters.medianFilter(imageBitmap)
+                        .subscribeOn(Schedulers.computation())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe { it ->
+                            ivSelectedImage.setImageBitmap(it)
+                        }
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposables.dispose()
     }
 }
